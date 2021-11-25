1398e0705ccf112953638121a66e199a93fc68df
freeman286 <molemayer@gmail.com>
2021-11-25 10:12:11 +0000

    Add depth measurement to generated data

--- Analysis/generator.py
+++ Analysis/generator.py
@@ -4,9 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pylab
+from scipy.spatial.transform import Rotation as R
 
 spool_radius = 37.5e-3
 sensor_error = np.deg2rad(0.1)
+pressure_error = 0.16e-3
 spool_offset = 0.281
 
 read_path = os.getcwd() + '/points/test.csv'
@@ -23,25 +25,30 @@ base_locations = [
     [2,0,0]
 ]
 
-base_locations_rotations = np.deg2rad([
-    [45 , 5],
-    [90 , -4],
-    [135 , -1]
-]) # Rotation about z and y axis according to the right hand rule
+water_level = 2
 
-def cart2sph(x, y, z, offset):
+base_rotations = np.deg2rad([
+    [45 , 5, -5],
+    [90 , -4, 3],
+    [135 , -1, -20]
+]) #Rotation about z, y and z axis according to the right hand rule (Euler angles)
 
-    # Some matrix rotations to get us in the right frame of reference
-    x_dash = np.cos(offset[1]) * (np.cos(offset[0]) * x + np.sin(offset[0]) * y) - np.sin(offset[1]) * z
-    y_dash = -np.sin(offset[0]) * x + np.cos(offset[0]) * y
-    z_dash = np.sin(offset[1]) * (np.cos(offset[0]) * x + np.sin(offset[0]) * y) + np.cos(offset[1]) * z
+pressure_offset = [0.100, 0.200, 0.200]
 
+def cart2sph(x, y, z, base_rotation, base_location):
+
+    #Some matrix rotations to get us in the right frame of reference
+    rotation = R.from_euler('zyz', -base_rotation)
+
+    [x_dash, y_dash, z_dash] = np.matmul(rotation.as_matrix(), np.array([x, y, z]).T)
+
+    d = (np.add(np.array(base_location).T, np.matmul(rotation.as_matrix(), np.array(pressure_offset).T)))[2] + water_level
     
     hxy = np.hypot(x_dash, y_dash)
     r = np.hypot(hxy, z_dash)
     el = np.arctan2(z_dash, hxy)
     az = np.arctan2(y_dash, x_dash)
-    return az, el, r
+    return az, el, r, d
 
 file_reader = csv.reader(read_file)
 
@@ -56,11 +63,13 @@ for reading in range(reading_count):
         
         x,y,z = np.subtract(points, np.array(base_locations[reading]))
 
-        az, el, r = cart2sph(x,y,z, base_locations_rotations[reading])
+        az, el, r, d = cart2sph(x,y,z, base_rotations[reading], base_locations[reading])
 
-        angles = np.degrees(np.add([az, el, (r-spool_offset)/(spool_radius)], np.random.uniform(low = -sensor_error, high = sensor_error, size=(1,3))))[0]
+        coords = np.degrees(np.add([az, el, (r-spool_offset)/(spool_radius)], np.random.uniform(low = -sensor_error, high = sensor_error, size=(1,3))))[0]
 
-        write_file.write(', '.join(map('{0:.2f}'.format, angles)) + '\n')
+        coords = np.append(coords, d)
+
+        write_file.write(', '.join(map('{0:.2f}'.format, coords)) + '\n')
 
         if (reading == 0):
             ax.scatter(points[0], points[1], points[2], color='red')
