abfdcd4edf9899251a06814a24c8fede896f56ad
freeman286 <molemayer@gmail.com>
2021-12-06 10:01:48 +0000

    Set up variables for least squares adjustment

--- Analysis/adjustment.py
+++ Analysis/adjustment.py
@@ -42,6 +42,12 @@ pressure_points = np.zeros((reading_count, 3))
 
 depths = np.zeros((reading_count))
 
+#Least squares adjustment
+k = np.ones((point_count, 3 * reading_count, 1)) #Residuals
+A = np.zeros((point_count, 3 * reading_count, 3)) #Adjustment shape
+m = np.zeros((point_count, 3 * reading_count, 1)) #Distance and angles in aligned frame of reference
+d = np.zeros((point_count, 3 * reading_count, 1)) #Mean distance and angles in aligned frame of reference (initial guess)
+
 def kabsch_rotated_points(input_points):
 
     input_centroid = np.mean(input_points, axis=0)
@@ -111,8 +117,28 @@ def mean():
 
     return means
 
-#def least_squares_adjustment():
-    
+def least_squares_adjustment_setup():
+    global k
+    for n in range(point_count):
+        ref_points = np.subtract(collated_points[n,:,:], base_points)
+        mean_ref_points = np.subtract(np.tile(means[n], (reading_count,1)), base_points)
+        
+        for reading in range(reading_count):
+            az, el, r = cart2sph(ref_points[reading, 0], ref_points[reading, 1], ref_points[reading, 2])
+            m[n, 3*reading, 0] = az
+            m[n, 3*reading+1, 0] = el
+            m[n, 3*reading+2, 0] = r
+
+            az, el, r = cart2sph(mean_ref_points[reading, 0], mean_ref_points[reading, 1], mean_ref_points[reading, 2])
+            d[n, 3*reading, 0] = az
+            d[n, 3*reading+1, 0] = el
+            d[n, 3*reading+2, 0] = r
+
+    k = np.subtract(m,d)
+         
+
+def least_squares_adjustment():
+    least_squares_adjustment_setup()
         
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -148,15 +174,16 @@ for line in file_reader:
         i += 1
 
 
-# Adjust everything to be in the same frame of reference as the water surface
-r, d = find_water_surface_transformation()
-aligned_points = np.subtract(np.dot(aligned_points, r.T), np.tile(np.array([[0,0,d]]), (reading_count * point_count, 1)))
-pressure_points = np.subtract(np.dot(pressure_points, r.T), np.tile(np.array([[0,0,d]]), (reading_count, 1)))
-base_points = np.subtract(np.dot(base_points, r.T), np.tile(np.array([[0,0,d]]), (reading_count, 1)))
+#Adjust everything to be in the same frame of reference as the water surface
+r, h = find_water_surface_transformation()
+aligned_points = np.subtract(np.dot(aligned_points, r.T), np.tile(np.array([[0,0,h]]), (reading_count * point_count, 1)))
+pressure_points = np.subtract(np.dot(pressure_points, r.T), np.tile(np.array([[0,0,h]]), (reading_count, 1)))
+base_points = np.subtract(np.dot(base_points, r.T), np.tile(np.array([[0,0,h]]), (reading_count, 1)))
 
 means = mean()
 
-ax.scatter(means[:,0], means[:,1], means[:,2], color='red', label='points')  
+least_squares_adjustment()
+ax.scatter(means[:,0], means[:,1], means[:,2], color='red', label='mean points')  
 ax.scatter(pressure_points[:,0], pressure_points[:,1], pressure_points[:,2], color='blue', label='pressure locations')
 ax.scatter(base_points[:,0], base_points[:,1], base_points[:,2], color='green', label='base locations')
 
