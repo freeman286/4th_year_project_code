703df8029e4c4d93a5ac41f92364bddf12f6d8cd
freeman286 <molemayer@gmail.com>
2022-01-13 16:29:53 +0000

    First try at ADC test

---
+++ Pi/mic_test.py
@@ -0,0 +1,14 @@
+import smbus
+import time
+
+# Get I2C bus
+bus = smbus.SMBus(1)
+
+bus.write_word_data(0x40, 0x40, 0x44) # Set register 0 to 1
+
+while (True) :
+    data = bus.read_byte(0x40)
+
+    volts = data / 4096000
+
+    print(volts)
