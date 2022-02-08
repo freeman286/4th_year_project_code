f19edee616204ebd571ce8d96a7a8bf1a4bde916
Joel Mayer <molemayer@gmail.com>
2022-02-08 15:28:05 +0000

    Configure program to run at boot

--- Pi/main.py
+++ Pi/main.py
@@ -11,7 +11,9 @@ import numpy as np
 from timeit import default_timer as timer
 from modules.sensors import *
 
-write_path = os.getcwd() + '/data/test.csv'
+time.sleep(60)
+
+write_path = os.getcwd() + '/data/data'+datetime.now().strftime("%Y.%m.%d.%H.%M.%S")+'.csv'
 write_file = open(write_path, "w")
 
 i = 0
@@ -42,26 +44,23 @@ ADC.ADS1256_SetDiffChannal(0)
 t = timer()
 ADC.ADS1256_StartContinuousADC()
 
-try:
-    while(1):
-        ADC_value = ADC.ADS1256_Read_ADC_Data_Continuous()
-        t = timer()
-        i=(i+1)%N
-        buffer[i] = ADC_value
+while(1):
+    ADC_value = ADC.ADS1256_Read_ADC_Data_Continuous()
+    t = timer()
+    i=(i+1)%N
+    buffer[i] = ADC_value
         
-        if (i==N-1):
-            angles = read_angles(angles)
+    if (i==N-1):
+        angles = read_angles(angles)
             
-            fft = np.fft.fft(np.multiply(buffer-np.mean(buffer), np.hamming(N)))/N
+        fft = np.fft.fft(np.multiply(buffer-np.mean(buffer), np.hamming(N)))/N
             
-            detection_level = alpha*abs(fft[scan_index])+(1-alpha)*detection_level   
+        detection_level = alpha*abs(fft[scan_index])+(1-alpha)*detection_level   
                 
-            if (detection_level>threshold):
-                print("detected")
-                depth = read_depth()
-                write_file.write(', '.join(map('{0:.2f}'.format, angles)) + ', {0:.4f}, '.format(depth) + str(datetime.now().time()) + '\n')
-                time.sleep(10)
+        if (detection_level>threshold):
+            print("detected")
+            depth = read_depth()
+            write_file.write(', '.join(map('{0:.2f}'.format, angles)) + ', {0:.4f}, '.format(depth) + datetime.now().strftime("%H.%M.%S") + '\n')
+            write_file.flush()
+            time.sleep(10)
             
-except KeyboardInterrupt:
-    write_file.close()
-    print("interrupted")
